a695b20b36378d7af7a6314fdf9c5809dbbe8dfc
lanang <lananglanusaputera@gmail.com>
2025-02-19 11:05:01 +0800

    add empty folder image

--- next.config.ts
+++ next.config.ts
@@ -11,4 +11,36 @@ const nextConfig: NextConfig = {
   },
 };
 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:all*(png|jpg|ico)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      // {
+      //   protocol: 'https',
+      //   hostname: process.env.NEXT_PUBLIC_HOSTNAME,
+      //   port: '',
+      //   pathname: '/**',
+      // },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
 export default nextConfig;
