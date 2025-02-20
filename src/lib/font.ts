5b1ec89ec31ab2d0f79647aade78a5ac6d3af7f8
lanang <lananglanusaputera@gmail.com>
2025-02-20 19:48:34 +0800

    fix font src

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -3,47 +3,47 @@ import localFont from "next/font/local";
 export const fontPoppins = localFont({
     src: [
         {
-            path: "../../public/fonts/poppins/Poppins-ExtraLightItalic.ttf",
+            path: "fonts/poppins/Poppins-ExtraLightItalic.ttf",
             weight: "100",
             style: "italic",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-Light.ttf",
+            path: "fonts/poppins/Poppins-Light.ttf",
             weight: "200",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-Regular.ttf",
+            path: "fonts/poppins/Poppins-Regular.ttf",
             weight: "300",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-Regular.ttf",
+            path: "fonts/poppins/Poppins-Regular.ttf",
             weight: "400",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-Medium.ttf",
+            path: "fonts/poppins/Poppins-Medium.ttf",
             weight: "500",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-SemiBold.ttf",
+            path: "fonts/poppins/Poppins-SemiBold.ttf",
             weight: "600",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-Bold.ttf",
+            path: "fonts/poppins/Poppins-Bold.ttf",
             weight: "700",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-ExtraBold.ttf",
+            path: "fonts/poppins/Poppins-ExtraBold.ttf",
             weight: "800",
             style: "normal",
         },
         {
-            path: "../../public/fonts/poppins/Poppins-Black.ttf",
+            path: "fonts/poppins/Poppins-Black.ttf",
             weight: "900",
             style: "normal",
         },
